512fd74c05b1867816d00e23afe2811fb1402937
TO <takahiro.ohori@gmail.com>
2023-01-29 21:53:37 +0900

    chore: add eslint, prettier settings

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
